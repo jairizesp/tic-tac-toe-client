3dc2ced092a408506a752ffad3aafad27a848abb
jairizesp <esposojairizedward@gmail.com>
2024-08-04 21:35:25 +0800

    change api url

--- src/constants/api/api.constants.ts
+++ src/constants/api/api.constants.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = import.meta.env.DEV
-  ? "http:localhost:3000"
-  : import.meta.env.VITE_REACT_API_URL;
+// export const BASE_URL = import.meta.env.DEV
+//   ? "http:localhost:3000"
+//   : import.meta.env.VITE_REACT_API_URL;
+export const BASE_URL = import.meta.env.VITE_REACT_API_URL;
